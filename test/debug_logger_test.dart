d871cff57dbe7c2f5c59233e76de493af4b30aa0
Hamid Marzbani <hamidreza.marzbani@nexttoptech.org>
2025-04-20 15:06:29 +0330

    test: Add tests for debug logger.

---
+++ test/debug_logger_test.dart
@@ -0,0 +1,25 @@
+import 'package:log_engine/src/debug_logger.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('DebugLogger Tests', () {
+    final logger = DebugLogger();
+
+    /// returnsNormally: Ensures that the given function or block of code executes without throwing any exceptions.
+    test('Log info message', () {
+      expect(() => logger.info('Info message'), returnsNormally);
+    });
+
+    test('Log debug message', () {
+      expect(() => logger.debug('Debug message'), returnsNormally);
+    });
+
+    test('Log warning message', () {
+      expect(() => logger.warn('Warning message'), returnsNormally);
+    });
+
+    test('Log error message', () {
+      expect(() => logger.error('Error message'), returnsNormally);
+    });
+  });
+}
